9c9ff0d02bc954f3987eb621dd0136a2191d1dbc
lalewis1 <lawson@kurrawong.ai>
2025-07-17 12:36:47 +1000

    feat: add nuxt-security module with CORS configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      esriApiKey: ""
+    modules: ["nuxt-security"],
+    security: {
+        corsHandler: {
+            origin: "*",
+            methods: "*",
+            allowHeaders: "*",
+            exposeHeaders: "*",
+        }
+    },
+    compatibilityDate: '2025-05-15',
+    devtools: {enabled: true},
+    runtimeConfig: {
+        public: {
+            portalUrl: "",
+            portalGroupId: "",
+            esriApiKey: ""
+        }
     }
-  }
 })
