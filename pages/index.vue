8b158c58706b0bdf650c271a3832ed9456f533e6
lalewis1 <lawson@kurrawong.ai>
2025-07-21 13:08:58 +1000

    proper item logging to console

--- pages/index.vue
+++ pages/index.vue
@@ -8,11 +8,13 @@
 
 <script setup lang="ts">
 onMounted(async () => {
-  const {default: esriConfig} = await import("@arcgis/core/config");
-  const {default: Map} = await import("@arcgis/core/Map");
-  const {default: MapView} = await import("@arcgis/core/views/MapView");
-  const {default: PortalGroup} = await import("@arcgis/core/portal/PortalGroup");
-  const {default: Portal} = await import("@arcgis/core/portal/Portal");
+  const { default: esriConfig } = await import("@arcgis/core/config");
+  const { default: Map } = await import("@arcgis/core/Map");
+  const { default: MapView } = await import("@arcgis/core/views/MapView");
+  const { default: PortalGroup } = await import(
+    "@arcgis/core/portal/PortalGroup"
+  );
+  const { default: Portal } = await import("@arcgis/core/portal/Portal");
 
   const config = useRuntimeConfig();
   esriConfig.apiKey = config.public.esriApiKey;
@@ -34,9 +36,13 @@ onMounted(async () => {
   const group = new PortalGroup({
     portal: portal,
     id: config.public.portalGroupId,
-  })
-  const items = await group.queryItems();
-  console.log(items);
+  });
+  const queryItems = await group.queryItems();
+  queryItems.results.forEach(async (queryItem) => {
+    await queryItem.load();
+    console.log(JSON.stringify(queryItem));
+    console.log(`'${queryItem.title}' is layer? ${queryItem.isLayer}`);
+  });
 
   const map = new Map({
     basemap: "arcgis/topographic",
@@ -48,7 +54,5 @@ onMounted(async () => {
     center: [-118.805, 34.027],
     zoom: 13,
   });
-
-
 });
 </script>
